18ae84a65f6ddaa611053e07c66f68e19b1317a3
cuvar <luca-mueller@gmx.net>
2023-08-13 11:05:03 +0200

    feat: unit value object

---
+++ server/domain/valueobject/unit.go
@@ -0,0 +1,38 @@
+package valueobject
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type Unit struct {
+	value    string
+}
+
+func NewUnit(value string) (*Unit, error) {
+	striped := strings.Replace(value, " ", "", -1)
+	if len(striped) == 0 {
+		return nil, errors.New("Unit must not be empty")
+	}
+	return &Unit{
+		value: striped,
+	}, nil
+}
+
+func (u *Unit) Value() string {
+	return u.value
+}
+
+func (u *Unit) Id() string {
+	return strings.ToLower(u.value)
+}
+
+func (u *Unit) Equals(other *Unit) bool {
+	return u.value == other.value
+}
+
+func (u *Unit) ToString() string {
+	s := fmt.Sprintf("%s", u.value);
+	return s;
+}
---
+++ server/domain/valueobject/unit_test.go
@@ -0,0 +1,94 @@
+package valueobject
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnitCreationPositive(t *testing.T) {
+	// arrange
+	value := "kg"
+
+	// act
+	q, err := NewUnit(value)
+	
+	// assert 
+	assert.NotNil(t, q)
+	assert.Nil(t, err)
+}
+
+func TestUnitCreationNegative(t *testing.T) {
+	// arrange
+	value := ""
+
+	// act
+	q, err := NewUnit(value)
+	
+	// assert 
+	assert.Nil(t, q)
+	assert.EqualError(t, err, "Unit must not be empty")
+}
+
+
+func TestUnitValue(t *testing.T) {
+	// arrange
+	value := "kg"
+	q, _ := NewUnit(value)
+
+	// act
+	amount := q.Value()
+	
+	// assert 
+	assert.Equal(t, value, amount)
+}
+
+func TestUnitId(t *testing.T) {
+	// arrange
+	value := "kg"
+	q, _ := NewUnit(value)
+
+	// act
+	id := q.Id()
+	
+	// assert 
+	assert.Equal(t, "kg", id)
+}
+
+func TestUnitEqualsPositive(t *testing.T) {
+	// arrange
+	value := "kg"
+	q, _ := NewUnit(value)
+	q2, _ := NewUnit(value)
+
+	// act
+	equals := q.Equals(q2)
+	
+	// assert 
+	assert.True(t, equals)
+}
+
+func TestUnitEqualsNegative(t *testing.T) {
+	// arrange
+	value := "kg"
+	q, _ := NewUnit(value)
+	q2, _ := NewUnit("g")
+
+	// act
+	equals := q.Equals(q2)
+	
+	// assert 
+	assert.False(t, equals)
+}
+
+func TestUnitToString(t *testing.T) {
+	// arrange
+	value := "kg"
+	q, _ := NewUnit(value)
+
+	// act
+	s := q.ToString()
+	
+	// assert 
+	assert.Equal(t, "kg", s)
+}
