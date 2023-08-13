5141510e334e32edc5d9e697f5bb85691355f710
cuvar <luca-mueller@gmx.net>
2023-08-13 11:09:35 +0200

    feat: ingredient value object

---
+++ server/domain/valueobject/ingredient.go
@@ -0,0 +1,38 @@
+package valueobject
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type Ingredient struct {
+	name    string
+}
+
+func NewIngredient(name string) (*Ingredient, error) {
+	striped := strings.Replace(name, " ", "", -1)
+	if len(striped) == 0 {
+		return nil, errors.New("Ingredient name must not be empty")
+	}
+	return &Ingredient{
+		name: striped,
+	}, nil
+}
+
+func (i *Ingredient) Name() string {
+	return i.name
+}
+
+func (i *Ingredient) Id() string {
+	return strings.ToLower(i.name)
+}
+
+func (i *Ingredient) Equals(other *Ingredient) bool {
+	return i.name == other.name
+}
+
+func (i *Ingredient) ToString() string {
+	s := fmt.Sprintf("%s", i.name);
+	return s;
+}
---
+++ server/domain/valueobject/ingredient_test.go
@@ -0,0 +1,94 @@
+package valueobject
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIngredientCreationPositive(t *testing.T) {
+	// arrange
+	value := "kg"
+
+	// act
+	q, err := NewIngredient(value)
+	
+	// assert 
+	assert.NotNil(t, q)
+	assert.Nil(t, err)
+}
+
+func TestIngredientCreationNegative(t *testing.T) {
+	// arrange
+	value := ""
+
+	// act
+	q, err := NewIngredient(value)
+	
+	// assert 
+	assert.Nil(t, q)
+	assert.EqualError(t, err, "Ingredient name must not be empty")
+}
+
+
+func TestIngredientName(t *testing.T) {
+	// arrange
+	value := "potatoes"
+	q, _ := NewIngredient(value)
+
+	// act
+	amount := q.Name()
+	
+	// assert 
+	assert.Equal(t, value, amount)
+}
+
+func TestIngredientId(t *testing.T) {
+	// arrange
+	value := "potatoes"
+	q, _ := NewIngredient(value)
+
+	// act
+	id := q.Id()
+	
+	// assert 
+	assert.Equal(t, "potatoes", id)
+}
+
+func TestIngredientEqualsPositive(t *testing.T) {
+	// arrange
+	value := "potatoes"
+	q, _ := NewIngredient(value)
+	q2, _ := NewIngredient(value)
+
+	// act
+	equals := q.Equals(q2)
+	
+	// assert 
+	assert.True(t, equals)
+}
+
+func TestIngredientEqualsNegative(t *testing.T) {
+	// arrange
+	value := "potatoes"
+	q, _ := NewIngredient(value)
+	q2, _ := NewIngredient("tomatoes")
+
+	// act
+	equals := q.Equals(q2)
+	
+	// assert 
+	assert.False(t, equals)
+}
+
+func TestIngredientToString(t *testing.T) {
+	// arrange
+	value := "potatoes"
+	q, _ := NewIngredient(value)
+
+	// act
+	s := q.ToString()
+	
+	// assert 
+	assert.Equal(t, "potatoes", s)
+}
